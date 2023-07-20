97533db8693ce538a15f17a215e13672c60726fa
Anderson <eusouanderson@outlook.com>
2023-07-20 09:22:22 -0300

    "update"

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,37 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.send_button)
-
-
-        button.setOnClickListener {
-            login()
-        }
-    }
-    private fun login(){
-        val logo = findViewById<ImageView>(R.id.logo)
-        val email = findViewById<TextView>(R.id.entry_email)
-        val password = findViewById<TextView>(R.id.entry_password)
-        val valueEmail = email.text.toString()
-        val valuePasswod = password.text.toString()
-        if (valueEmail == "eusouanderson"){
-            logo.visibility = View.GONE
-        }
-        else if (valuePasswod == "123"){
-            logo.visibility = View.GONE
-        }
-    }
-    private fun clear(){
-
-        val logo = findViewById<ImageView>(R.id.logo)
-        val email = findViewById<TextView>(R.id.entry_email)
-        val password = findViewById<TextView>(R.id.entry_password)
-        logo.visibility = View.GONE
-        email.visibility = View.GONE
-        password.visibility = View.GONE
 
     }
 }
-
 
 
