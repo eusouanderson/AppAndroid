5c397edf7ee93197d72a54bf1983104e89c27d6b
Anderson <eusouanderson@outlook.com>
2023-07-24 13:06:38 -0300

    "update"

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.example.myapplication
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,13 +6,15 @@ import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
-import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import android.content.Intent
 import android.net.Uri
-
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var viewsToHide: List<View> = listOf()
+    private var viewsToSee: List<View> = listOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,49 +22,65 @@ class MainActivity : AppCompatActivity() {
 
         val container = findViewById<View>(R.id.container)
         val logo = findViewById<ImageView>(R.id.logo)
-
         val email = findViewById<TextInputLayout>(R.id.txt_input_email)
         val password = findViewById<TextInputLayout>(R.id.txt_input_password)
-
         val nome_cadastro = findViewById<TextInputLayout>(R.id.txt_input_nome_CADASTRO)
         val email_cadastro = findViewById<TextInputLayout>(R.id.txt_input_email_CADASTRO)
         val password_cadastro = findViewById<TextInputLayout>(R.id.txt_input_password_CADASTRO)
         val password_confirme_cadastro = findViewById<TextInputLayout>(R.id.txt_input_password_confirme_CADASTRO)
-
         val botao_login = findViewById<AppCompatButton>(R.id.btlogin)
-        val botao_cadastro = findViewById<AppCompatButton>(R.id.btn_cadastrar)
-
+        val botao_ir_cadastro = findViewById<AppCompatButton>(R.id.btn_ir_cadastrar)
+        val botao_cadastrar = findViewById<AppCompatButton>(R.id.btn_cadastrar)
         val text_cadastro = findViewById<TextView>(R.id.txtTelaCadastro)
         val progress_bar = findViewById<ProgressBar>(R.id.progressBar)
-
         val footer = findViewById<View>(R.id.footer)
+        val git = findViewById<ImageView>(R.id.imageView2)
+        val newValue = 1800
 
-        val newvalor = 1800
+        viewsToHide = listOf(email, password, botao_ir_cadastro, botao_login, text_cadastro, git)
+        viewsToSee = listOf(nome_cadastro, email_cadastro, password_cadastro, password_confirme_cadastro, botao_cadastrar)
 
         //botão login
         botao_login.setOnClickListener {
             val screenTwo = Intent(this,ScreenTwo::class.java)
             startActivity(screenTwo)
         }
-        botao_cadastro.setOnClickListener {
-            footer.layoutParams.height = newvalor
+        botao_ir_cadastro.setOnClickListener {
+            hideViews()
+            showViews()
+            footer.layoutParams.height = newValue
             footer.requestLayout()
-            botao_login.visibility = View.GONE
-            botao_cadastro.visibility = View.GONE
-            email.visibility = View.GONE
-            password.visibility = View.GONE
-            nome_cadastro.visibility = View.VISIBLE
-            nome_cadastro.bringToFront()
-            email_cadastro.visibility = View.VISIBLE
-            email_cadastro.bringToFront()
-            password_cadastro.visibility = View.VISIBLE
-            password_cadastro.bringToFront()
-            password_confirme_cadastro.visibility = View.GONE
-            password_confirme_cadastro.bringToFront()
+        }
 
+        botao_cadastrar.setOnClickListener{
+            register()
         }
     }
+    private fun hideViews() {
+        for (view in viewsToHide) {
+            view.visibility = View.GONE
+        }
+    }
+    private fun showViews(){
+        for (view in viewsToSee){
+            view.visibility = View.VISIBLE
+        }
+    }
+    private fun register(){
+        val botao_cadastrar = findViewById<AppCompatButton>(R.id.btn_cadastrar)
+        val password_cadastro = findViewById<EditText>(R.id.edit_password_cadastro)
+        val confirme_password = findViewById<EditText>(R.id.edit_password_confirme)
+        val value_password = password_cadastro.text.toString()
+        val conf_password = confirme_password.text.toString()
 
+
+        if (value_password == conf_password) {
+            val mainActivity = Intent(this, MainActivity::class.java)
+            startActivity(mainActivity)
+        } else {
+            Toast.makeText(this, "Senhas não coincidem!", Toast.LENGTH_LONG).show()
+        }
+    }
     fun openWebsite(view: View) {
         // URL do site
         val websiteUrl = "https://github.com/eusouanderson"
@@ -77,9 +94,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
-
 }
-
 
 
 
