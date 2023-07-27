cb3e2d0d48121791a150985fe5c828b14ba4b377
Anderson <eusouanderson@outlook.com>
2023-07-26 21:44:13 -0300

    "updated"

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.myapplication
+
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
--- app/src/main/java/com/example/myapplication/ScreenTwo.kt
+++ app/src/main/java/com/example/myapplication/ScreenTwo.kt
@@ -1,31 +1,47 @@
 package com.example.myapplication
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.google.android.material.textfield.TextInputLayout
+import com.google.gson.Gson
 import org.json.JSONObject
 import javax.net.ssl.HttpsURLConnection
 import java.net.URL
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class ScreenTwo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen_two)
         val text = findViewById<TextView>(R.id.textView)
-        val butoon = findViewById<Button>(R.id.button)
+        val button = findViewById<Button>(R.id.button)
 
-        butoon.setOnClickListener {
+        button.setOnClickListener {
             Thread {
                 val url = URL("https://server-eqbe.onrender.com/api/usuarios")
                 val conn = url.openConnection() as HttpsURLConnection
+                val nome_cadastro = findViewById<TextInputLayout>(R.id.txt_input_nome_CADASTRO)
+
 
                 try {
                     val data = conn.inputStream.bufferedReader().readText()
 
-                    runOnUiThread {
-                        text.text = data
+                    val gson = Gson()
+                    val usersList = gson.fromJson(data, Array<User>::class.java)
+
+                    for (user in usersList) {
+                        val name = user.name
+                        val email = user.email
+                        val password = user.password
+
+                        runOnUiThread {
+                            text.text = "Ola $name, $email, $password, $nome_cadastro"
+                        }
                     }
+
                 } finally {
                     conn.disconnect()
                 }
@@ -33,6 +49,8 @@ class ScreenTwo : AppCompatActivity() {
         }
     }
 }
+
+data class User(val name: String, val email: String, val password: String)
 
 
 
